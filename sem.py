3130530057f6e0b686f48d9f7371b709141ebf59
Danlu14 <daniel01022002159@gmail.com>
2024-10-14 22:06:27 -0500

    LA FEEEEEEEEEEEEE

--- sem.py
+++ sem.py
@@ -1,5 +1,7 @@
-#Ejercicio 1: Imprimir 10 veces "Hola mundo"
+import streamlit as st
+
+# Ejercicio 1: Imprimir 10 veces "Hola mundo"
 st.subheader("Ejercicio 1: imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar ejercicio 1"):
-    for i in range (10):
+    for i in range(10):
         st.write("Hola Mundo")
