74a21111f264b5980baaab5161e601ac9489a3e6
Danlu14 <daniel01022002159@gmail.com>
2024-11-03 14:14:59 -0500

    excersice 3

---
+++ IDL3/EJERCICIO3.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+pares = [i for i in range(20, 401, 2)]
+
+# Calcular el promedio
+suma_pares = sum(pares)
+cantidad_pares = len(pares)
+promedio = suma_pares / cantidad_pares
+
+# Mostrar el resultado 
+st.write("Los números pares entre 20 y 400 son:")
+st.write(pares)
+st.write(f"El promedio de estos números pares es: {promedio}")
